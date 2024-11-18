5618512d4271d2ec62ce356c6de8019aa699d184
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-11-18 09:30:53 +0100

    auth errand

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+import * as express from 'express';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Middleware to parse raw body for Stripe webhooks
+  // app.use('/stripe/webhook', express.raw({ type: 'application/json' }));
+
+  // Default JSON body parser for other routes
+  app.use(express.json());
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  });
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT') || 3000;
+  await app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Running in ${configService.get<string>('NODE_ENV')} on port ${PORT}`);
+  });
 }
 bootstrap();
