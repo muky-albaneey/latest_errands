96dfc6878fa10ca53634f9148684aad9a6a038ce
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2025-01-29 18:20:00 +0100

    ADDING API FOR NIN AND DRIVERS LICENCE

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,20 @@
-/* eslint-disable prettier/prettier */
-
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios'; // Import HttpModule
 import { User } from './entities/auth.entity';
 import { Card } from './entities/card.entity';
 import { DiverLicense } from './entities/license.entity';
 import { Nin } from './entities/drive.entity';
-import { HttpService } from '@nestjs/axios';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Card,DiverLicense, Nin]),
-    ConfigModule, 
+    TypeOrmModule.forFeature([User, Card, DiverLicense, Nin]),
+    ConfigModule,
+    HttpModule, // Add HttpModule here
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
@@ -25,8 +24,8 @@ import { HttpService } from '@nestjs/axios';
       inject: [ConfigService],
     }),
   ],
-  exports: [TypeOrmModule],
+  exports: [TypeOrmModule, HttpModule], // Export HttpModule if needed in other modules
   controllers: [AuthController],
-  providers: [AuthService, HttpService],
+  providers: [AuthService], // Remove HttpService from providers
 })
 export class AuthModule {}
