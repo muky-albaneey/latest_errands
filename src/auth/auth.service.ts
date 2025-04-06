383680bc39634a86fde7ce1c54a5c9b921a49ef2
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2025-04-06 17:28:00 +0100

    Adding linode codes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -82,7 +82,13 @@ export class AuthService {
       ContentType: file.mimetype, // Set content type (e.g., image/jpeg)
       ACL: 'public-read', // Make the file publicly accessible
     };
-
+    console.log('Bucket name:', this.bucketName);
+    console.log('Uploading file:', {
+      originalname: file.originalname,
+      mimetype: file.mimetype,
+      size: file.size,
+    });
+    
     try {
       const uploadResult = await this.s3.upload(params).promise();
       return uploadResult.Location; // Return the URL of the uploaded file
