021e4bdf4f660e24d4d7368d688fc4b0bc44ce45
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2025-04-24 04:17:06 +0100

    ADDING CHARGES SECTION

--- src/rides/rides.module.ts
+++ src/rides/rides.module.ts
@@ -9,10 +9,11 @@ import { Ride } from './entities/ride.entity';
 import { DriverEarning } from './entities/driverEarnings.entity';
 import { User } from 'src/auth/entities/user.entity';
 import { WithdrawalRequest } from './entities/withdrawalRequest.entity';
+import { Charge } from 'src/charges/entities/charge.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Ride, DriverEarning, WithdrawalRequest, User]),
+        TypeOrmModule.forFeature([Ride, DriverEarning, WithdrawalRequest, User, Charge]),
         ConfigModule,
         HttpModule,
       ],
